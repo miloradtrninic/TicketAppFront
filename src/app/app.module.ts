5c8add70c2d24a877f253b9de56359e57ae9e510
milosrs <milosribar@yahoo.com>
2018-02-01 01:26:08 +0100

    Front end za Login i Registraciju gotov.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing-module/app-routing.module';
@@ -28,10 +30,12 @@ import { UserPanelComponent } from './components/user-panel/user-panel.component
     ReservationComponent,
     OneClickTicketComponent,
     AdminPanelComponent,
-    UserPanelComponent
+    UserPanelComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [AppRoutingModule],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  const uri='';
+  const regUri='';
+  const loginUri='';
 
-  constructor() { }
+
+  let regInfo = {
+    password: '',
+    repeatPW: '',
+    name: '',
+    surname: '',
+    email: '',
+    phoneno: '',
+    city: '',
+  };
+
+  let logInfo = {
+    email: '',
+    password: '',
+  };
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+  }
+
+  tryRegister() {
+    this.clearAllInfo();
+    console.log("Spawn register clicked");
+  }
+
+  cancelClick(){
+    this.clearAllInfo();
+    console.log("Cancel clicked, cleared fields.");
+  }
+
+  clearAllInfo() {
+    this.regInfo.password='';
+    this.regInfo.repeatPW='';
+    this.regInfo.name='';
+    this.regInfo.surname='';
+    this.regInfo.email='';
+    this.regInfo.phoneno='';
+    this.logInfo.password='';
+  }
+
+  tryLogin() {
+    this.clearAllInfo();
+    console.log("Spawn login clicked");
   }
 
 }
