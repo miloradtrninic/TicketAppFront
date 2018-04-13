c1b028a46281f745306717ad96e8651bda684362
milosrs <milosribar@yahoo.com>
2018-04-13 18:18:12 +0200

    Dodate preview modeli. Treba preimenovati fajlove.

---
+++ src/app/components/top-level/top-level.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {HelperFunctions} from '../../shared/util/helper-functions';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-top-level',
+  templateUrl: './top-level.component.html',
+  styleUrls: ['./top-level.component.css']
+})
+export class TopLevelComponent implements OnInit {
+
+  constructor(protected service: UserService, protected router: Router, protected redirectUrl: string) { }
+
+  ngOnInit() {
+    if (!HelperFunctions.isEmptyValue(window.localStorage.getItem('currentUser'))) {
+      this.service.setLoginInfo();
+      if (!HelperFunctions.isEmptyValue(this.redirectUrl)) {
+        this.router.navigate([this.redirectUrl]);
+      }
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+
+}
