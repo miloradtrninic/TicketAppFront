82a029cdc7addbce6aea3f432b298ff7553837e2
milosrs <milosribar@yahoo.com>
2018-04-17 03:21:31 +0200

    WIP HomePanel

--- src/app/app-routing/routing.module.ts
+++ src/app/app-routing/routing.module.ts
@@ -30,6 +30,7 @@ import { ReservationComponent } from '../components/reservation/reservation.comp
 import { TerminComponent } from '../components/termin/termin.component';
 import { OnlyAdminGuard } from './guards/only-admin-guard';
 import { OnlyLoggedInGuard } from './guards/only-logged-in-guard';
+import {FriendsComponent} from '../components/friends/friends.component';
 import { MovieListComponent } from '../components/cinema/movie/movie-list/movie-list.component';
 
 
@@ -48,6 +49,7 @@ const routes: Routes = [
   { path: 'theatre/:id/new', component: AddPlayComponent},
   { path: 'theatre/:id/play/:playId', component: TheatreComponent},
   { path: 'fanzone', component: FanZoneComponent},
+  { path: 'friends', component: FriendsComponent},
   { path: 'admin-panel', component: AdminPanelComponent, /*canActivate: [OnlyAdminGuard],*/ children: [
     {path: '', component: HomeAdminComponent},
     {path: 'fan-zone', component: AdminFanZoneComponent},
