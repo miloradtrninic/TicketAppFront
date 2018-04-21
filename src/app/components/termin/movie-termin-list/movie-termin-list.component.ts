a001b5c770ce17aa60a96201977d24dc859e4a08
Sara Corak <saracorak0210@gmail.com>
2018-04-21 04:25:28 +0200

    Brza rezervacija

--- src/app/components/termin/movie-termin-list/movie-termin-list.component.ts
+++ src/app/components/termin/movie-termin-list/movie-termin-list.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Termin } from '../../../model/termin.model';
 import { TerminService } from '../../../services/termin.service';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../../../services/movie.service';
 import { Movie } from '../../../model/movie.model';
+import { NgForm } from '@angular/forms';
+import { Seating } from '../../../model/seating.model';
 
 @Component({
   selector: 'app-movie-termin-list',
@@ -12,13 +14,18 @@ import { Movie } from '../../../model/movie.model';
 })
 export class MovieTerminListComponent implements OnInit {
 
-
+  reserved: Array<Seating>
   message: string;
   movies : Movie[] = [];
   allowP = true;
   cinemaId: number;
   movie: Movie;
-  constructor(public terminService : TerminService, public movieService: MovieService,public route: ActivatedRoute) { }
+  @ViewChild('reserveForm') form: NgForm;
+
+  constructor(public terminService : TerminService, public movieService: MovieService,public route: ActivatedRoute) {
+    this.reserved = new Array();
+
+   }
 
   ngOnInit() {
     console.log("olalal" + this.movies);
@@ -37,6 +44,32 @@ export class MovieTerminListComponent implements OnInit {
       );
 
   }
+
+  reserveTermin(){
+
+  }
+
+  toArray(number): Array<number> {
+    const array = new Array<number>();
+    for (let i = 0; i < number; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+
+  reserveSeat(row: number, column: number) {
+    const foundIndex = this.findSeat(row, column);
+    if (foundIndex === -1) {
+      this.reserved.push(new Seating(row, column, true));
+    } else {
+      this.reserved.splice(foundIndex, 1);
+    }
+  }
+
+  findSeat(row: number, column: number) {
+    return this.reserved.findIndex(s => s.row === row && s.number === column);
+  }
+  
   allowPreview1() {
     this.allowP = false;
   }
